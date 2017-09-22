dfffc02fe6c18d4151fa4f739fe8a1c47e8d39f1
Denis Valdivia <devalfe@gmail.com>
2017-09-22 14:02:58 -0500

    tabla atractivos

--- database/seeds/AtractivosTableSeeder.php
+++ database/seeds/AtractivosTableSeeder.php
@@ -108,11 +108,7 @@ class AtractivosTableSeeder extends Seeder
         atractivo::create([
             'titulo' => 'COMPLEJO SAN FRANCISCO Y LAS CATACUMBAS',
             'descripcion' => 'Se ubica en la calle Lima, a una cuadra de la plaza de armas.',
-            'detalle' => 'Se ubica en la calle Lima, a una cuadra de la plaza de armas. El antiguo complejo de San Francisco fue edificado por los Jesuitas y luego de su expulsión fue ocupado por los Franciscanos. Desde su inicio funcionó para actividades docentes y misioneras y posteriormente como convento de varones. Fue sede del antiguo colegio Propaganda Fide. Destruido y levantado después de desastrosos sismos, aún mantiene la mayor parte de su estructura de piedra y el ingreso a las Catacumbas. De la antigua Iglesia de San Francisco hoy solo quedan viejas paredes derruidas.
-    Es una edificación de piedra, confeccionada en tres niveles, aunque en la actualidad se puedan observar el primero y parte de los corredores subterráneos. Estuvo compuesto de colegio, hospedaje para estudiantes, convento e iglesia de tres naves. Al ingresar podemos observar un sobrio patio de piedra. 
-    Bajo la vieja urbe de Moquegua, en lo que era la Villa de Santa Catalina de Guadalcázar, se halla un conjunto de primer orden llamado Catacumbas, dispuesto en torno a los complejos eclesiásticos del virreinato (C. San Francisco, C. Betlemítico y C. Santo Domingo), Iglesia Matriz, el antiguo corregimiento y los solares de las principales familias con poder político de la antigua villa. 
-    Las Catacumbas, bóvedas subterráneas confeccionadas en piedra, son de simple manufactura y con pequeños nichos. Son pasillos subterráneos que tuvieron doble fin: la comunicación interna entre las órdenes religiosas y políticas, y los enterramientos habituales, no sólo de religiosos, sino también de personajes prominentes.
-    Algo similar, aunque en menor tamaño, se puede observar en la villa heroica de San Agustín de Torata, sus catacumbas, están confeccionadas en piedra provenientes de las canteras circundantes.',
+            'detalle' => 'Se ubica en la calle Lima, a una cuadra de la plaza de armas. El antiguo complejo de San Francisco fue edificado por los Jesuitas y luego de su expulsión fue ocupado por los Franciscanos. Desde su inicio funcionó para actividades docentes y misioneras y posteriormente como convento de varones. Fue sede del antiguo colegio Propaganda Fide. Destruido y levantado después de desastrosos sismos, aún mantiene la mayor parte de su estructura de piedra y el ingreso a las Catacumbas. De la antigua Iglesia de San Francisco hoy solo quedan viejas paredes derruidas. Es una edificación de piedra, confeccionada en tres niveles, aunque en la actualidad se puedan observar el primero y parte de los corredores subterráneos. Estuvo compuesto de colegio, hospedaje para estudiantes, convento e iglesia de tres naves. Al ingresar podemos observar un sobrio patio de piedra. Bajo la vieja urbe de Moquegua, en lo que era la Villa de Santa Catalina de Guadalcázar, se halla un conjunto de primer orden llamado Catacumbas, dispuesto en torno a los complejos eclesiásticos del virreinato (C. San Francisco, C. Betlemítico y C. Santo Domingo), Iglesia Matriz, el antiguo corregimiento y los solares de las principales familias con poder político de la antigua villa.  Las Catacumbas, bóvedas subterráneas confeccionadas en piedra, son de simple manufactura y con pequeños nichos. Son pasillos subterráneos que tuvieron doble fin: la comunicación interna entre las órdenes religiosas y políticas, y los enterramientos habituales, no sólo de religiosos, sino también de personajes prominentes. Algo similar, aunque en menor tamaño, se puede observar en la villa heroica de San Agustín de Torata, sus catacumbas, están confeccionadas en piedra provenientes de las canteras circundantes.',
             'direccion' => '',
             'ubicacion' => 'Mariscal Nieto',
             'longitud' => ,
@@ -136,9 +132,77 @@ class AtractivosTableSeeder extends Seeder
             'categoria_id' => 1]);
 
         atractivo::create([
-            'titulo' => '',
-            'descripcion' => '',
-            'detalle' => '',
+            'titulo' => 'CASA DE LOS CABELLO',
+            'descripcion' => 'En este antiguo solar nació la escritora Mercedes Cabello de la Llosa en 1842.',
+            'detalle' => 'En este antiguo solar nació la escritora Mercedes Cabello de la Llosa en 1842. La vivienda fue restaurada después del terremoto de 1868, manteniendo su anterior diseño. Goza de cuartos adyacentes a la puerta principal y una ventana de hierro forjado a la calle. La fachada luce un muro de piedra. Se ingresa por un zaguán con techo abovedado que conduce a un patio empedrado y en el cual había una pileta de piedra. Una amplia escalera al lado izquierdo del patio conduce al segundo piso. Las habitaciones del segundo nivel son de quincha, con techos en mojinete de caña y barro; un corredor de madera y tres balcones.',
+            'direccion' => 'Calle Moquegua N° 838',
+            'ubicacion' => 'Mariscal Nieto',
+            'longitud' => ,
+            'latitud' => ,
+            'horarios' => '',
+            'foto_url' => 'img',
+            'activo' => 1,
+            'categoria_id' => 1]);  
+
+        atractivo::create([
+            'titulo' => 'CASA DE LOS BARRIOS O CASA DE LA CULTURA',
+            'descripcion' => 'Esta casa fue construida por don Pedro de Arguedas en 1791, luce su tradicional balcón de madera.',
+            'detalle' => 'Esta casa fue construida por don Pedro de Arguedas en 1791, luce su tradicional balcón de madera. En 1824 fue adquirida por don Camilo Barrios Nieto, en completo estado de deterioro; se ocupó de restaurarla y ampliarla. Su nieto don Domingo Barrios y su esposa doña Adelaida Hurtado de Mendoza donan la propiedad a la municipalidad para que en ella funcionen las oficinas. Tiene dos patios alrededor de los cuales se distribuyen las amplias habitaciones con techos altos en mojinete. Hoy el Municipio la ha convertido en un elegante centro cultural.',
+            'direccion' => 'Calle Moquegua N° 851',
+            'ubicacion' => 'Mariscal Nieto',
+            'longitud' => ,
+            'latitud' => ,
+            'horarios' => '',
+            'foto_url' => 'img',
+            'activo' => 1,
+            'categoria_id' => 1]);   
+            
+        atractivo::create([
+            'titulo' => 'CASA DE LOS FERNÁNDEZ MALDONADO',
+            'descripcion' => 'Fue reconstruida por Mariano Fernández Maldonado, cuyas iniciales aparecen grabadas en la parte superior de la portada.',
+            'detalle' => 'Fue reconstruida por Mariano Fernández Maldonado, cuyas iniciales aparecen grabadas en la parte superior de la portada. Más conocida como Casa de la serpiente, sus antiguos propietarios la llamaban Casa de la Culebra, a causa de la cabeza de serpiente tallada debajo de una pequeña ventana de la fachada. En un principio la vivienda fue de dos niveles, el terremoto de 1868 destruyó este antiguo solar y el segundo piso no se volvió a levantar. Las espaciosas habitaciones tienen vigas de madera artísticamente talladas y es el único caso de una vivienda que presenta actualmente dichas características. Sirvió de cuartel durante la guerra con Chile.',
+            'direccion' => 'Calle Tarapacá N° 398',
+            'ubicacion' => 'Mariscal Nieto',
+            'longitud' => ,
+            'latitud' => ,
+            'horarios' => '',
+            'foto_url' => 'img',
+            'activo' => 1,
+            'categoria_id' => 1]);   
+            
+        atractivo::create([
+            'titulo' => 'CASA DE LOS DIEZ CANSECO Y FERNÁNDEZ DÁVILA',
+            'descripcion' => 'De este importante solar hoy sólo puede observarse la bella portada de piedra ricamente tallada personalizada con querubines, plantas, animales y figuras heráldicas.',
+            'detalle' => 'De este importante solar hoy sólo puede observarse la bella portada de piedra ricamente tallada personalizada con querubines, plantas, animales y figuras heráldicas.',
+            'direccion' => 'Calle Ayacucho N 868',
+            'ubicacion' => 'Mariscal Nieto',
+            'longitud' => ,
+            'latitud' => ,
+            'horarios' => '',
+            'foto_url' => 'img',
+            'activo' => 1,
+            'categoria_id' => 1]); 
+
+        atractivo::create([
+            'titulo' => 'CASA DE LOS JIMÉNEZ DE LA FLOR',
+            'descripcion' => 'Hoy funciona el hogar de ancianos.',
+            'detalle' => 'Hoy funciona el hogar de ancianos. Es un solar característico de Moquegua, con techos en mojinete, portada de piedra y rejas sevillanas. El antiguo zaguán nos conduce a un amplio patio empedrado, rodeado de habitaciones, algunas de las cuales aún lucen habitaciones enchapadas en madera; el segundo patio luce una fuente de piedra, a su vez, rodeado de habitaciones.',
+            'direccion' => 'Calle Tarapacá N',
+            'ubicacion' => 'Mariscal Nieto',
+            'longitud' => ,
+            'latitud' => ,
+            'horarios' => '',
+            'foto_url' => 'img',
+            'activo' => 1,
+            'categoria_id' => 1]);
+
+
+        atractivo::create([
+            'titulo' => 'COMPLEJO BETLEMÍTICO Y EL BARRIO ',
+            'descripcion' => 'Ubicado a cuatro cuadras de la plaza de armas, con dirección este',
+            'detalle' => 'Ubicado a cuatro cuadras de la plaza de armas, con dirección este. Fue una imponente edificación que contó con una iglesia sencilla, pero de bella arquitectura. Convento y hospital con campanario, sacristía, botica y portería. Fundado en 1743, fue uno de los hospitales más antiguos del Perú virreinal. 
+El barrio que circunda al antiguo complejo es conocido como Barrio Belén. Encantador contorno que guarda la arquitectura virreinal y republicana de Moquegua, sus solares exhiben balcones, portadas, ventanas sevillanas y techos de mojinete.
+',
             'direccion' => '',
             'ubicacion' => 'Mariscal Nieto',
             'longitud' => ,
@@ -146,7 +210,7 @@ class AtractivosTableSeeder extends Seeder
             'horarios' => '',
             'foto_url' => 'img',
             'activo' => 1,
-            'categoria_id' => 1]);            
- 
+            'categoria_id' => 1]);
+
     }
 }
