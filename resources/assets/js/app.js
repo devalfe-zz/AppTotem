aaf7675b15430b649cea2cfe474fe14fdb64b6a0
Denis Valdivia <devalfe@gmail.com>
2017-10-05 17:43:05 -0500

    uso de vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,10 +3,18 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import router from './routes.js';
 
 //require('./bootstrap');
 
-//window.Vue = require('vue');
+
+window.Vue = require('vue');
+Vue.use(require('vue-resource'));
+Vue.http.headers.common['X-CSRF-TOKEN'] = document.getElementById('csrf_token').value;
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
 window.$ = window.jQuery = require('jquery');
 
 function include(scriptUrl) {
@@ -24,11 +32,35 @@ function isIE() {
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-// Vue.component('example', require('./components/Example.vue'));
+Vue.component('example', require('./components/Example.vue'));
 
-// const app = new Vue({
-//     el: '#app'
-// });
+const app = new Vue({
+    el: '#app',
+    router
+});
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 function displaySection(nav) {
     var sections = new Array(5);
