782cc5784d0574ff97e9858afb4d1502fd7dc1c8
Delix <mdt.webmaster@gmail.com>
2018-05-08 20:14:21 -0500

    acces cros

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,5 +18,9 @@ class Cors
         return $next($request)
         ->header('Access-Control-Allow-Origin','*')
         ->header('Access-Control-Allow-Methods','GET');
+        ->header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+
+
     }
 }
