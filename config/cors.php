919ae2a912a3b67f338f6d00f30775a1ecba33aa
Devalfe <mdt.webmaster@gmail.com>
2019-09-01 11:03:29 -0500

    cors update

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
      * A cors profile determines which origins, methods, headers are allowed for
      * a given requests. The `DefaultProfile` reads its configuration from this
@@ -16,27 +15,20 @@ return [
      * This configuration is used by `DefaultProfile`.
      */
     'default_profile' => [
-
         'allow_credentials' => false,
 
         'allow_origins' => [
             'https://guia-turistica.munimoquegua.gob.pe/public/api/v1/',
+            '*'
         ],
 
-        'allow_methods' => [
-            'POST',
-            'GET',
-            'OPTIONS',
-            'PUT',
-            'PATCH',
-            'DELETE',
-        ],
+        'allow_methods' => ['POST', 'GET', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
 
         'allow_headers' => [
             'Content-Type',
             'X-Auth-Token',
             'Origin',
-            'Authorization',
+            'Authorization'
         ],
 
         'expose_headers' => [
@@ -45,17 +37,17 @@ return [
             'Content-Type',
             'Expires',
             'Last-Modified',
-            'Pragma',
+            'Pragma'
         ],
 
         'forbidden_response' => [
             'message' => 'Forbidden (cors).',
-            'status' => 403,
+            'status' => 403
         ],
 
         /*
          * Preflight request will respond with value for the max age header.
          */
-        'max_age' => 60 * 60 * 24,
-    ],
+        'max_age' => 60 * 60 * 24
+    ]
 ];
